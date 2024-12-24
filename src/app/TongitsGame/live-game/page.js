6b7245fa290969e86a3ea50c743cf2dea8a9d0ed
jaypee1414 <jhnplbncyrn03@gmail.com>
2024-12-25 07:55:34 +0800

    add : gamebet file

---
+++ src/app/TongitsGame/live-game/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Gamebet from './gamebet'
+function page() {
+  return (
+    <div>
+      <Gamebet/>
+    </div>
+  )
+}
+
+export default page
--- src/utils/card-utils.js
+++ src/utils/card-utils.js
@@ -8,12 +8,6 @@ const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
  */
 
 
-export function createCard(suit, rank) {
-    if (!suits.includes(suit) || !ranks.includes(rank)) {
-        throw new Error('Invalid suit or rank');
-    }
-    return { suit, rank };
-}
 
 export function createDeck() {
     const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
